b4fd7dcaf4cce4a4ecf0a0dc2ed0c08f67d1d299
deborabastos <deborabastos14@gmail.com>
2023-02-09 00:55:10 -0300

    sub

--- js/script.js
+++ js/script.js
@@ -10,17 +10,32 @@ function handleButtonClick() {
     var inputN1 = document.querySelector('#first-num')
     var inputN2 = document.querySelector('#second-num')
     var sumResult = document.querySelector('#sum-result')
+    var subABResult = document.querySelector('#subAB-result')
+    var subBAResult = document.querySelector('#subBA-result')
 
     var n1 = Number(inputN1.value);
     var n2 = Number(inputN2.value);
 
     var sum = calcSum(n1, n2);
+    var subAB = calcSubAB(n1, n2);
+    var subBA = calcSubBA(n1, n2);
+
 
     sumResult.textContent = sum;
+    subABResult.textContent = subAB;
+    subBAResult.textContent = subBA;
 }
 
 function calcSum(n1 ,n2){
-    return n1 + n2;
+    return (n1 + n2);
+}
+
+function calcSubAB(n1 ,n2){
+    return (n1 - n2);
+}
+
+function calcSubBA(n1 ,n2){
+    return (n2 - n1);
 }
 
 start()
